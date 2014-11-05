b446f3dafec93ea51c5e1bc4cf5fbfb9b6a1c146
Pablo Carle <pablo.carle@gmail.com>
2014-11-04 23:41:25 -0300

    Commit inicial proyecto Java

---
+++ TP/src/Test.java
@@ -0,0 +1,13 @@
+import Implementaciones.Matriz;
+import TDA.MatrizTDA;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		MatrizTDA<Integer> nuevaMatriz = new Matriz<Integer>();
+		//Hacer algo
+	}
+}
---
+++ TP/src/impl/NuevaMatriz.java
@@ -0,0 +1,31 @@
+package impl;
+
+import TDA.MatrizTDA;
+
+public class NuevaMatriz<E> implements MatrizTDA<E> {
+
+	@Override
+	public void inicializarMatriz(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int obtenerDimension() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public E obtenerValor(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setearValor(int arg0, int arg1, E arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

940f37c933fd11df7991f71d3f2fddf89c017a05
Pablo <pablocarle@users.noreply.github.com>
2014-11-06 10:35:20 -0300

    Update NuevaMatriz.java

--- TP/src/impl/NuevaMatriz.java
+++ TP/src/impl/NuevaMatriz.java
@@ -3,24 +3,24 @@ package impl;
 import TDA.MatrizTDA;
 
 public class NuevaMatriz<E> implements MatrizTDA<E>{
-	E[][] matriz;
+	private E[][] matriz;
 	
+	@Override
 	public void inicializarMatriz(int arg0){
 		matriz = new E[arg0][arg0];
 	}
 
+	@Override
 	public int obtenerDimension() {
-		int i;
-		int j;
-		for (i = 1; obtenerValor(i, 1) != null;i++);
-		for (j = 1; obtenerValor(1, j) != null;j++);
-		return i*j;
+		return matriz.length;
 	}
-
+	
+	@Override
 	public E obtenerValor(int arg0, int arg1) {
 		return matriz[arg0][arg1];
 	}
-
+	
+	@Override
 	public void setearValor(int arg0, int arg1, E arg2) {
 		matriz[arg0][arg1] = arg2;
 	}
